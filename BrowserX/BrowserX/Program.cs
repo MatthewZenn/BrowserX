cc420103f88ca1611e14387acfa571d8085ce205
Matthew Zenn <spyguy57@gmail.com>
2021-09-03 15:14:19 -0400

    Added HTTPRequest

--- BrowserX/BrowserX/Program.cs
+++ BrowserX/BrowserX/Program.cs
@@ -1,23 +1,42 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace BrowserX
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+
+        private static async Task ProcessRepositories(string url)
         {
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine(" ______                               __   __");
-            Console.WriteLine(" | ___ \\                              \\ \\ / /");
-            Console.WriteLine(" | |_/ /_ __ _____      _____  ___ _ __\\ V / ");
-            Console.WriteLine(" | ___ \\ '__/ _ \\ \\ /\\ / / __|/ _ \\ '__/   \\ ");
-            Console.WriteLine(" | |_/ / | | (_) \\ V  V /\\__ \\  __/ | / /^\\ \\");
-            Console.WriteLine(" \\____/|_|  \\___/ \\_/\\_/ |___/\\___|_| \\/   \\/");
-            Console.WriteLine("                                             ");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync(url);
+            var msg = await stringTask;
+
+            Console.Write(msg);
+        }
+
+        static async Task Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" ________                                          ____  __");
+            Console.WriteLine(" ___  __ )_______________      ______________________  |/ /");
+            Console.WriteLine(" __  __  |_  ___/  __ \\_ | /| / /_  ___/  _ \\_  ___/_    /");
+            Console.WriteLine(" _  /_/ /_  /   / /_/ /_ |/ |/ /_(__  )/  __/  /   _    |");
+            Console.WriteLine(" /_____/ /_/    \\____/____/|__/ /____/ \\___//_/    /_/|_|");
+            Console.WriteLine("                                                          ");
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write(" §: ");
             Console.ForegroundColor = ConsoleColor.White;
             string input = Console.ReadLine();
+            await ProcessRepositories(input);
         }
     }
 }
